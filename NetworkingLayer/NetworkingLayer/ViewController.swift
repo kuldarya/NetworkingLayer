f7b5b6b80ae183d282d28a6432a2aaaaf991a612
Darya Kuliashova <kuldiand@gmail.com>
2022-05-17 15:47:50 +0200

    Create empty project

---
+++ NetworkingLayer/NetworkingLayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkingLayer
+//
+//  Created by Darya Kuliashova on 17/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
